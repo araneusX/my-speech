a1ce43ef9da39f1e187e3df68f36b17fafdb7777
Alexander Oleshkevich <araneusx@gmail.com>
2020-07-21 13:59:22 +0300

    feat: no mp3

--- components/speech.tsx
+++ components/speech.tsx
@@ -6,8 +6,8 @@ import AudioRecorder from 'audio-recorder-polyfill';
 import mpegEncoder from 'audio-recorder-polyfill/mpeg-encoder';
 import { variables } from '../constants';
 
-AudioRecorder.encoder = mpegEncoder;
-AudioRecorder.prototype.mimeType = 'audio/mpeg';
+// AudioRecorder.encoder = mpegEncoder;
+// AudioRecorder.prototype.mimeType = 'audio/mpeg';
 window.MediaRecorder = AudioRecorder;
 
 const Speech = () => {
