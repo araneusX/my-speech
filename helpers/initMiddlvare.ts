437371688efceeaf3e8b1c84cf2a09357dc51229
Alexander Oleshkevich <araneusx@gmail.com>
2020-08-03 14:47:21 +0300

    fix: fix any

--- helpers/initMiddlvare.ts
+++ helpers/initMiddlvare.ts
@@ -1,6 +1,6 @@
-export default function initMiddleware(middleware) {
-  return (req, res) => new Promise((resolve, reject) => {
-    middleware(req, res, (result) => {
+export default function initMiddleware(middleware: any) {
+  return (req: any, res: any) => new Promise((resolve, reject) => {
+    middleware(req, res, (result: any) => {
       if (result instanceof Error) {
         return reject(result);
       }
